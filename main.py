34aef7b4e3150281fd3d9022a4c623be837c4d85
VDhriti <dhritiverma02@gmail.com>
2020-11-12 09:53:53 +0530

    added suggestion functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import xlrd
 
-loc = ('C:\\Users\\dhrit\\Desktop\\Availability from 15 SepTuesday.xlsx')
+loc = ('.\\Test Excel.xlsx')
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
@@ -9,31 +9,87 @@ sheet = wb.sheet_by_index(0)
 lastRow = sheet.nrows
 names = []
 days = []
+mealTimes = []
 
-for i in range(2, lastRow):
-    if (sheet.cell_value(i, 1) != ''):
-        names.append(sheet.cell_value(i, 1).lower())
+for h in range(2, lastRow):
+    if (sheet.cell_value(h, 1) != ''):
+        names.append(sheet.cell_value(h, 1).lower())
 
-    Value = sheet.cell_value(i, 2)
-    if (Value != ''):
-        if Value == 'All':
-            days.append(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'])
+for i in range(2, len(names)+2):
+    Value = sheet.cell_value(i, 2).lower()
+    if Value == 'all':
+        days.append(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'])
+    elif Value == 'weekdays':
+        days.append(['mon', 'tue', 'wed', 'thu', 'fri'])
+    elif Value == 'weekends':
+        days.append(['sat', 'sun'])
+    elif '+' in Value:
+        Value.replace(' ', '')
+        tempList = Value.split('+')
+        days.append(tempList)
+    elif Value == '':
+        days.append(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'])
+    else:
+        days.append(Value)
+   
+    mealTimes.append(
+        [
+            sheet.cell_value(i, 3).lower(), 
+            sheet.cell_value(i, 4).lower(), 
+            sheet.cell_value(i, 5).lower()
+        ]
+    )   
+    
 
-        elif Value == 'Weekdays':
-            days.append(['mon', 'tue', 'wed', 'thu', 'fri'])
-
-        elif Value == 'Weekends':
-            days.append(['sat', 'sun'])
-
-        elif '+' in Value:
-            tempList = Value.split('+').lower()
-            days.append(tempList)
-
-        else:
-            days.append(Value.lower())
-        
 rowDict = {}
-for i in range(len(names)):
-    rowDict[names[i]] = days[i]
+for j in range(len(names)):
+    rowDict[j] = [names[j], days[j], mealTimes[j]]
 
-#print(rowDict)
+
+startDay = input("Please enter the first three letters of the start day: ").lower()
+daysOfTheWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+dayIndex = daysOfTheWeek.index(startDay)               # monday = 0, tuesday = 1 and so on
+
+dayCounter = 0
+bSpec, bAll, lSpec, lAll, dSpec, dAll = [], [], [], [], [], []
+
+while dayCounter < 10:
+    b, l, d = [bSpec, bAll], [lSpec, lAll], [dSpec, dAll]
+    avMeals = [b, l, d]
+    
+    for k in range(len(names)):
+        if daysOfTheWeek[dayIndex] in rowDict[k][1]:
+            #pplAvailableThisDay.append(rowDict[k][0]
+            for l in range(3):
+                if rowDict[k][2][l] == 'y':
+                    if rowDict[k][1] == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']:
+                        avMeals[l][1].append(rowDict[k][0])
+                    else:
+                        avMeals[l][0].append(rowDict[k][0])
+             
+    print('Day', dayCounter+1, ':', daysOfTheWeek[dayIndex].capitalize())
+    print('   ', 'Breakfast: ')
+    print('   ', '   ', 'Specific Availability: ', bSpec)
+    print('   ', '   ', 'Free for All', bAll)
+    print('')
+
+    print('   ', 'Lunch: ')
+    print('   ', '   ', 'Specific Availability: ', lSpec)
+    print('   ', '   ', 'Free for All', lAll)
+    print('')
+
+    print('   ', 'Dinner: ')
+    print('   ', '   ', 'Specific Availability: ', dSpec)
+    print('   ', '   ', 'Free for All', dAll)
+    print('')
+
+
+    bSpec, bAll, lSpec, lAll, dSpec, dAll = [], [], [], [], [], []
+
+    print('')
+    print('-------------------------------------')
+    print('')
+
+    dayIndex = (dayIndex + 1)%7
+    dayCounter+=1
